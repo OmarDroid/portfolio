78df8a170055d532cda758fbbbeb6163e9b69666
Omar <andomaroid1@gmail.com>
2024-08-17 12:19:29 +0200

    fine tuning
    - edit urls
    - remove unwanted components
    - code cleanups

--- src/app/components/ProjectsSection.jsx
+++ src/app/components/ProjectsSection.jsx
@@ -10,78 +10,68 @@ const projectsData = [
     title: "Payback App",
     description: "Mobile payment & reward program",
     image: "./images/projects/1.png",
-    gitUrl: "/",
-    previewUrl: "/",
+    previewUrl: "https://www.payback.de/app",
   },
   {
     id: 2,
     title: "BVB FC",
     description: "Borussia Dortmund official mobile app",
     image: "./images/projects/2.png",
-    gitUrl: "/",
-    previewUrl: "/",
+    previewUrl: "https://play.google.com/store/apps/details?id=de.bvb09.android&hl=en",
   },
   {
     id: 3,
     title: "Westham FC",
     description: "Westham official mobile app",
     image: "./images/projects/3.png",
-    gitUrl: "/",
-    previewUrl: "/",
+    previewUrl: "https://play.google.com/store/apps/details?id=de.elasticbrains.west_ham_united&hl=en_GB&gl=US",
   },
   {
     id: 4,
     title: "Navvis Ivion Go",
     description: "Indoor Mapping",
     image: "./images/projects/4.png",
-    gitUrl: "/",
-    previewUrl: "/",
+    previewUrl: "https://play.google.com/store/apps/details?id=com.navvis.mdfa&hl=en&gl=US",
   },
   {
     id: 5,
     title: "Bde Bank",
     description: "Mobile banking app",
     image: "./images/projects/5.png",
-    gitUrl: "/",
-    previewUrl: "/",
+    previewUrl: "https://play.google.com/store/apps/details?id=com.setelia.android.bde",
   },
   {
     id: 6,
     title: "BLC Bank",
     description: "Mobile payment app",
     image: "./images/projects/6.png",
-    gitUrl: "/",
-    previewUrl: "/",
+    previewUrl: "https://www.blcbank.com/personal/detail.aspx?pid=164&ptid=1",
   },
   {
     id: 7,
     title: "Elnashra",
     description: "Mobile news app",
     image: "./images/projects/7.png",
-    gitUrl: "/",
-    previewUrl: "/",
+    previewUrl: "https://play.google.com/store/apps/details?id=com.elnashra&hl=en&gl=US",
   },
   {
     id: 8,
-    title: "Hey BLC",
+    title: "Hey Pay",
     description: "Mobile payment app",
     image: "./images/projects/8.png",
-    gitUrl: "/",
-    previewUrl: "/",
+    previewUrl: "http://hey-pay.me/#aboutUsId",
   },
   {
     id: 9,
     title: "NBK Bank",
     description: "Mobile banking app",
     image: "./images/projects/9.png",
-    gitUrl: "/",
-    previewUrl: "/",
+    previewUrl: "https://play.google.com/store/apps/details?id=com.nbk.IBGmobile&hl=en_US",
   },
 ];
 
 const ProjectsSection = () => {
   const ref = useRef(null);
-  const isInView = useInView(ref, { once: true });
 
   return (
     <section id="projects">
@@ -90,14 +80,13 @@ const ProjectsSection = () => {
       </h2>
 
       <ul ref={ref} className="grid md:grid-cols-3 gap-8 md:gap-12">
-        {projectsData.map((project, index) => (
+        {projectsData.map((project, _index) => (
          
             <ProjectCard
               key={project.id}
               title={project.title}
               description={project.description}
               imgUrl={project.image}
-              gitUrl={project.gitUrl}
               previewUrl={project.previewUrl}
             />
         ))}
