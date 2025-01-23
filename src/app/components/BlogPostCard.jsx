5371e9f5dea8bf495d0fb02d757f713850addd1d
Omar <andomaroid1@gmail.com>
2025-01-23 17:00:44 +0100

    adjust css

--- src/app/components/BlogPostCard.jsx
+++ src/app/components/BlogPostCard.jsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 const BlogPostCard = ({ article }) => {
     return (
         <div
-            className="h-52 md:h-72 mt-4 mb-8 rounded-xl relative group "
+            className="h-52 md:h-72 mt-4 mb-20 rounded-xl relative group "
             style={{ background: `url(${article.link})`, backgroundSize: "cover", backgroundPositionX: "center" }}
         >
             <Link
@@ -16,13 +16,12 @@ const BlogPostCard = ({ article }) => {
                     src={article.description.match(/<img[^>]+src="([^">]+)"/)?.[1]}
                     alt={article.title}
                     className="card-img-top img-fluid"
-                    width={250}
+                    width={400}
                     height={200}
                 />
 
-                <div className="text-white rounded-b-xl mt-3 bg-[#181818]py-6 px-4">
-                    <h5 className="text-xl font-semibold mb-2">{article.title}</h5>
-                    <p className="text-[#ADB7BE]">{new Date(article.pubDate).toLocaleDateString()}</p>
+                <div className="text-white rounded-b-l mt-3 bg-[#181818]py-3 px-0">
+                    <p className="text-m font-semibold fs-5 mb-2 text-wrap text-break">{article.title}</p>
                 </div>
 
 
