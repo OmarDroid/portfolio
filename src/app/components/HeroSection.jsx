a443412f0a4c37ab02c9c3844d7da7e79c71ef0a
Omar <andomaroid1@gmail.com>
2025-01-23 16:38:49 +0100

    Upgrade tailwind

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -15,7 +15,7 @@ const HeroSection = () => {
           className="col-span-8 place-self-center text-center sm:text-left justify-self-start"
         >
           <h2 className="text-white mb-4 text-2xl sm:text-xl lg:text-3xl lg:leading-normal font-extrabold">
-            <span className="text-4xl sm:text-2xl lg:text-6xl text-transparent bg-clip-text bg-gradient-to-r from-primary-400 to-secondary-600">
+            <span className="text-4xl sm:text-2xl lg:text-6xl text-transparent bg-clip-text bg-linear-to-r from-primary-400 to-secondary-600">
               Hello, I&apos;m Omar{" "}
             </span>
             <br></br>
@@ -42,7 +42,7 @@ const HeroSection = () => {
           transition={{ duration: 0.5 }}
           className="col-span-4 place-self-center mt-4 lg:mt-0"
         >
-          <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[300] lg:h-[250px] relative">
+          <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-300 lg:h-[250px] relative">
             <Image
               src="./images/hero-image.png"
               alt="hero image"
--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -43,14 +43,14 @@ const Navbar = () => {
           {!navbarOpen ? (
             <button
               onClick={() => setNavbarOpen(true)}
-              className="flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white"
+              className="flex items-center px-3 py-2 border rounded-sm border-slate-200 text-slate-200 hover:text-white hover:border-white"
             >
               <Bars3Icon className="h-5 w-5" />
             </button>
           ) : (
             <button
               onClick={() => setNavbarOpen(false)}
-              className="flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white"
+              className="flex items-center px-3 py-2 border rounded-sm border-slate-200 text-slate-200 hover:text-white hover:border-white"
             >
               <XMarkIcon className="h-5 w-5" />
             </button>
