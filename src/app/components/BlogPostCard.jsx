3f7601e29aa775c187c3eff5e849284da1886e4f
Omar <andomaroid1@gmail.com>
2025-01-23 16:44:37 +0100

    Adjust image

--- src/app/components/BlogPostCard.jsx
+++ src/app/components/BlogPostCard.jsx
@@ -2,7 +2,6 @@
 import React from "react";
 import Link from "next/link";
 
-
 const BlogPostCard = ({ article }) => {
     return (
         <div
--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -42,7 +42,7 @@ const HeroSection = () => {
           transition={{ duration: 0.5 }}
           className="col-span-4 place-self-center mt-4 lg:mt-0"
         >
-          <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-300 lg:h-[250px] relative">
+          <div className="rounded-full bg-[#181818] w-[250px] h-[250px] relative">
             <Image
               src="./images/hero-image.png"
               alt="hero image"
