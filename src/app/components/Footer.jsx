d10df3435d3e9f7a63ce4c77a4489dff01deb096
Omar <andomaroid1@gmail.com>
2025-01-23 17:57:43 +0100

    update footer

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -2,9 +2,10 @@ import React from "react";
 
 const Footer = () => {
   return (
+    
     <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white">
       <div className="container p-12 flex justify-center">
-        <p className="text-slate-600">© Copyright 2024 Omar Hamid</p>
+        <p className="text-slate-600">© Copyright 2025 Omar Hamid</p>
       </div>
     </footer>
   );
